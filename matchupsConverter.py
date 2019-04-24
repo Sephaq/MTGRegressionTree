099407ce0b49ecbdba17b66281986cfed6dc6d85
Marvin Goncalves de Aquino <marvin.aquino@nkey.com.br>
2019-04-24 19:12:42 -0300

    new converter

--- matchupsConverter.py
+++ matchupsConverter.py
@@ -10,26 +10,25 @@ with open ('decksDatabase.csv') as csv_file:
             # print(f'Colum names are {",".join(row)}')
             line_count += 1
         else:
-            # print(f'\t No {row[0]} de {row[1]} jogador: {row[2]}, com baralho {row[3]}, contra baralho: {row[4]} e oponente {row[5]}, com resultado V = {row[6]}, D = {row[7]}.  ')
-            deck = row[3]
-            print(deck)
-            opponent = row[4]
-            print(opponent)
-            victories = int(row[6])
-            print(victories)
-            defeats = int(row[7])
-            print(defeats)
+            deck = row[5]
+            # print(deck)
+            opponent = row[7]
+            # print(opponent)
+            victories = int(row[10])
+            # print(victories)
+            defeats = int(row[11])
+            # print(defeats)
             games = victories + defeats
-            print(games)
+            # print(games)
             gameFormat = row[0]
-            print(gameFormat)
+            # print(gameFormat)
             decks.append(deckMatchup(gameFormat,deck,opponent,games,victories,defeats))
             decks.append(deckMatchup(gameFormat,opponent,deck,games,defeats,victories))
             line_count += 1
 
 print(f'Processed {line_count} lines')
-print('Teste')
-print(f'Deck: {decks[0].archetype}, Matchup: {decks[0].oponentArchetype}, Games:{decks[0].games}, Victories:{decks[0].victories}, Losses:{decks[0].losses}')
+# print('Teste')
+# print(f'Deck: {decks[0].archetype}, Matchup: {decks[0].oponentArchetype}, Games:{decks[0].games}, Victories:{decks[0].victories}, Losses:{decks[0].losses}')
 
 with open('matchupsDatabase.csv',mode='w') as csv_file:
     outfile = csv.writer(csv_file)
